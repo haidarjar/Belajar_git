db653544d59739a3dda9c2fb186527b22457ae3d
Haidar azhar <idarmiaw@gmai.com>
2024-05-22 08:38:40 +0700

    Nambahin fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,5 +1,8 @@
 const add = (a, b) => a + b;
-console.log(add(1,6));
+console.log(add(1, 6));
 
-const subtrack = (a, b) => a-b;
-console.log(subtrack(5,3));
+const subtrack = (a, b) => a - b;
+console.log(subtrack(5, 3));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(5, 4));
