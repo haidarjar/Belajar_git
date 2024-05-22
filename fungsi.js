64b89b99cabac91c69775fdaa138ea8f96a66185
Haidar azhar <idarmiaw@gmai.com>
2024-05-22 08:57:40 +0700

    Merevisi commit perkalian

--- fungsi.js
+++ fungsi.js
@@ -1,11 +1,11 @@
 const add = (a, b) => a + b;
-console.log(add(1, 6));
+console.log(add(1,6));
 
 const subtrack = (a, b) => a - b;
 console.log(subtrack(5, 3));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(5, 4));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(5, 4));
 
 const devide = (a, b) => a / b;
 console.log(devide(9, 3));
